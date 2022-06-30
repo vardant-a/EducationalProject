eb8470cb8ef254b1bd07f9980c0c80ce4266c2d0
Aleksei Lokhin <lokhin.a.a@gmail.com>
2022-06-30 22:18:10 +0300

    Initial Commit

---
+++ EducationalProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EducationalProject
+//
+//  Created by Алексей on 30.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
