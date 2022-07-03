5488dbceba758a21f2d6cd00060c677d5978dedf
Aleksei Lokhin <lokhin.a.a@gmail.com>
2022-07-03 16:13:35 +0300

    Реализованы AlertControllers Реализованы 2 View

---
+++ EducationalProject/LogScreenViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LogScreenViewController.swift
+//  EducationalProject
+//
+//  Created by Алексей on 03.07.2022.
+//
+
+import UIKit
+
+class LogScreenViewController: UIViewController {
+    
+
+}
--- EducationalProject/ViewController.swift
+++ EducationalProject/ViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - IB Outlets
+    
+    @IBOutlet var loginUser: UITextField!
+    @IBOutlet var password: UITextField!
+    
+    // MARK: - Override Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    // MARK: - IB Actions AllertControllers
+    
+    @IBAction func showAllertUserName(_ sender: UIButton) {
+        let alert = UIAlertController(
+            title: "Oops?",
+            message: "Your name is Aleksei",
+            preferredStyle: .alert
+        )
+        let okButton = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+        
+        alert.addAction(okButton)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func showAllertUserPassword(_ sender: UIButton) {
+        let alert = UIAlertController(
+            title: "Oops?",
+            message: "Your password is Swiftbook",
+            preferredStyle: .alert
+        )
+        let okButton = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+        
+        alert.addAction(okButton)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func showAllert(_ sender: UIButton) {
+        let alert = UIAlertController(
+            title: "Invalid login or password",
+            message: "Please, enter corrent login and password",
+            preferredStyle: .alert
+        )
+        let okButton = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+        
+        alert.addAction(okButton)
+        present(alert, animated: true, completion: nil)
+    }
 }
 
